92b5e996d40e2108825a48910a0b97b55ca6255c
olaide-hok <olaidekareem43@yahoo.com>
2022-12-03 21:05:26 +0100

    Create NewTodoProps type.
    Change NewTodo component to use generics.

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,16 @@
 import React, {useRef} from 'react'
 
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void
+}
 
-const NewTodo: React.FC = () => {
+const NewTodo: React.FC<NewTodoProps> = (props) => {
     const textInputRef = useRef<HTMLInputElement>(null)
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault()
         const enteredText = textInputRef.current!.value
+        props.onAddTodo(enteredText)
     }
 
   return (
