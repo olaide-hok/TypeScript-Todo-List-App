5268a31a33f12626206becc36cd9874f5458658b
olaide-hok <olaidekareem43@yahoo.com>
2022-12-03 21:22:12 +0100

    Create State management for Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.models';
 
 function App() {
-  const todos = [{ id: 't1', text: 'Finish the course'}]
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const todoAddHandler = (text: string) => {
     console.log(text);    
